0381ac26b7d7bd23831e8a4ae1cfbc84734eb002
gaku <behuman.2501@gmail.com>
2020-11-08 01:25:52 +0900

    add about page

---
+++ pages/about.tsx
@@ -0,0 +1,17 @@
+import { FC } from 'react'
+import Layout from '../components/layout'
+
+const About: FC = (props) => {
+  return (
+    <div>
+      <Layout>
+        <h2>About</h2>
+        <section>
+            <p>this is about page.</p>
+        </section>
+      </Layout>
+    </div>
+  )
+}
+
+export default About
